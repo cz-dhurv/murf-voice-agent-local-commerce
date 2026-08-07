1d336c8089f4253e1fc718b16a1a7a80f411b983
cz-dhurv <dhruvkr209@example.com>
2026-08-07 22:36:40 +0530

    feat(agent): day2 — switch to Anisha voice (hi-IN, 11 Indian languages) for better code-mixed support

--- backend/src/agent.py
+++ backend/src/agent.py
@@ -116,8 +116,8 @@ async def my_agent(ctx: JobContext):
         # Text-to-speech (TTS) is your agent's voice, turning the LLM's text into speech that the user can hear
         # See all available models as well as voice selections at https://docs.livekit.io/agents/models/tts/
         tts=murf.TTS(
-                voice="Samar", 
-                locale="en-IN",
+                voice="Anisha",
+                locale="hi-IN",
                 style="Conversation",
                 tokenizer=tokenize.basic.SentenceTokenizer(min_sentence_len=2),
                 text_pacing=True
